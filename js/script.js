045a71e9783b833cc4a5ecda73d5e06d942561d5
Kingofbeer123 <kiril.shevchenko123@gmail.com>
2022-04-06 15:53:06 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ let a = prompt ('Введите первое число');
 let operator = prompt ('Введите действие');
 let b = prompt ('Введите второе число');
 let result;
-Number.isNaN(a,b) != true;
-if (operator == '+') {
+if (isNaN(a,b)) {
+    alert( `Введите корректные числа`);
+}
+else if (operator == '+') {
     alert( `Ваш ответ ${result = (a+b)}!`);
 }
 else if (operator == '-') { 
