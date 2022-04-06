7dcca62fc238a3b7cc661b34909d48f8ede58073
Kingofbeer123 <kiril.shevchenko123@gmail.com>
2022-04-06 15:35:39 +0300

    ne zakon4il

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-let name = prompt ('Hello! What you name?');
-alert( `Hello, ${name}! How are you?`);
+let a = prompt ('Введите первое число');
+let operator = prompt ('Введите действие');
+let b = prompt ('Введите второе число');
+let result;
+Number.isNaN(a,b) != true;
+if (operator == '+') {
+    alert( `Ваш ответ ${result = (a+b)}!`);
+}
+else if (operator == '-') { 
+    alert( `Ваш ответ ${result = (a-b)}!`);
+}
+else if (operator == '*') { 
+    alert( `Ваш ответ ${result = (a*b)}!`);
+}
+else if (operator == '/') { 
+    alert( `Ваш ответ ${result = (a/b)}!`);
+}
+else {
+    alert( `Такой операции не существует`);
+}
